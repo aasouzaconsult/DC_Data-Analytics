7db05496b310d49d18b064822df10fca59a8e441
Alex Souza <aasouzaconsult@gmail.com>
2023-08-15 21:06:15 -0300

    Add files via upload

---
+++ Transformacoes/projeto_data_warehouse_vendas/etls/script-job_vendas.cmd
@@ -0,0 +1,8 @@
+cls
+c:
+cd\Users\danie\OneDrive\Digital College\Material Compartilhado\Unidade 02\ETL\projeto_data_warehouse_vendas\etls
+echo on
+call kitchen /file:"C:\Users\danie\OneDrive\Digital College\Material Compartilhado\Unidade 02\ETL\projeto_data_warehouse_vendas\etls\job_vendas.kjb" /level Basic 
+echo off
+exit
+
